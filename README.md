92b8eef96c9e028afeecff61b346fcc0f910bf1d
DUPROS Leegan <leegan.dupros@epitech.eu>
2023-02-20 18:09:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dleeganportfolio
+ dleegan WebSite
---
+++ src/Components/TabBar/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  tabBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderColor: '#1E2D3D',
+    width: '100%',
+  },
+});
+
+export default styles;
