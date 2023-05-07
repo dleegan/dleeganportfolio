ed8acd04ef91cc81b54aabc715d59fff23800631
DUPROS Leegan <leegan.dupros@epitech.eu>
2023-05-07 18:01:42 +0200

    Update detail-screen.tsx

--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -1,17 +1,9 @@
-import { createParam } from 'solito'
-import { TextLink } from 'solito/link'
-import { Text } from 'app/design/typography'
-import { View } from 'app/design/view'
+import { Text, View } from 'react-native'
 
-const { useParam } = createParam<{ id: string }>()
-
-export function UserDetailScreen() {
-  const [id] = useParam('id')
-
+export function Test() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="mb-4 text-center font-bold">{`User ID: ${id}`}</Text>
-      <TextLink href="/">👈 Go Home</TextLink>
+    <View>
+      <Text>lalal</Text>
     </View>
   )
 }
