0bd33633604e783c3f627366aa665db45020eea5
DUPROS Leegan <leegan.dupros@epitech.eu>
2023-07-07 10:52:02 +0200

    [+] minor fixes

--- packages/app/Utils/Infos/InfosFiles.ts
+++ packages/app/Utils/Infos/InfosFiles.ts
@@ -3,6 +3,9 @@ import Formations from './portfolio/formations.json'
 
 import Experience5 from './portfolio/professionalXP/2023-présent.json'
 import Experience4 from './portfolio/professionalXP/2021-2023.json'
+import Experience3 from './portfolio/professionalXP/2021.json'
+import Experience2 from './portfolio/professionalXP/2020.json'
+import Experience from './portfolio/professionalXP/2019-2020.json'
 
 export default {
   bio: Bio,
@@ -11,10 +14,9 @@ export default {
   //expériences-professionnelles files
   ['2023-présent']: Experience5,
   ['2021-2023']: Experience4,
-  // { fileName: '2021-2023' },
-  // { fileName: '2021' },
-  // { fileName: '2020' },
-  // { fileName: '2019-2020' },
+  2021: Experience3,
+  2020: Experience2,
+  ['2019-2020']: Experience,
 }
 
 export let DataSchema = [
